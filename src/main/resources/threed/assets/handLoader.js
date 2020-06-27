a4d67149b56871ee8fdfe2e1886ee0ee071482f8
onukura <y.bibos.ishikawa@gmail.com>
2020-06-27 19:14:19 +0900

    fix loader #1

--- src/main/resources/threed/assets/handLoader.js
+++ src/main/resources/threed/assets/handLoader.js
@@ -19,14 +19,12 @@ function init(obj3D, div, ctr) {
 
   // Grid Setup
   var grid = new THREE.GridHelper( 20, 20, 0xffffff, 0x555555 );
-  grid.rotateOnAxis( new THREE.Vector3( 1, 0, 0 ), 90 * ( Math.PI/180 ) );
+  grid.rotateOnAxis( new THREE.Vector3( 1, 0, 0 ), Math.PI / 2 );
   scene.add( grid );
 
   // Camera Setup
   camera = new THREE.PerspectiveCamera( fov, divRatio, 1, 1000 );
   camera.up.set( 0, 0, 1 );
-  camera.position.set( 0, -9, 6 );
-  camera.lookAt( grid );
   pointlight = new THREE.PointLight( 0xffffff, 2, 0 )
   pointlight.castShadow = true;
   camera.add( pointlight );
@@ -39,6 +37,14 @@ function init(obj3D, div, ctr) {
   renderer.setSize( divW, divH );
   div.appendChild( renderer.domElement );
 
+  // Setup OrbitControls
+  var controls = new THREE.OrbitControls( camera, renderer.domElement );
+  controls.minDistance = 1;
+  controls.maxDistance = 30;
+  controls.enableKeys = false;
+  controls.addEventListener( 'change', render );
+  window.addEventListener( 'resize', onWindowResize(divW, divH), false );
+
   // Loader Setup
   var usewireframe = false
   if ( ctr[0].checked ) {
@@ -48,21 +54,22 @@ function init(obj3D, div, ctr) {
   var material = new THREE.MeshPhongMaterial( { color: 0x0e2045, specular: 0x111111, shininess: 200 , wireframe: usewireframe} );
   loader.load( obj3D, function ( geometry ) {
     var mesh = new THREE.Mesh( geometry, material );
-    mesh.position.set( 0, 0, 0 );
-    mesh.rotation.set( 0, 0, 0 );
-    mesh.scale.set( .02, .02, .02 );
+    mesh.rotation.set(0, 0, -Math.PI);
     mesh.castShadow = true;
     mesh.receiveShadow = true;
     scene.add( mesh );
     render();
+    const radius = mesh.geometry.boundingSphere.radius;
+    const scale = 4 / radius;
+    mesh.geometry.computeBoundingSphere();
+    mesh.scale.set(scale, scale, scale);
+    mesh.position.x += scale * mesh.geometry.boundingSphere.center.x;
+    mesh.position.y += scale * mesh.geometry.boundingSphere.center.y;
+    const dist = radius / (Math.sin((fov * (Math.PI / 180)) / 2));
+    camera.position.set( 0, dist + 1, dist );
+    controls.update();
+    camera.lookAt(grid);
   });
-
-  // Add controller
-  var controls = new THREE.OrbitControls( camera, renderer.domElement );
-  controls.addEventListener( 'change', render );
-  controls.target.set( 0, 1.2, 2 );
-  controls.update();
-  window.addEventListener( 'resize', onWindowResize(divW, divH), false );
 
 }
 
